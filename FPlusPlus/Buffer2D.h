2e5a548b95fbc9a51d4a3a4e438bfa84263f8372
Santiago Pacheco <pachesantiago@gmail.com>
2023-12-29 22:08:56 -0500

    Updated submodules

--- FPlusPlus/Buffer2D.h
+++ FPlusPlus/Buffer2D.h
@@ -1,21 +1,13 @@
 #pragma once
 #include <algorithm>
-
-// Clamp is only on c++ 17 :(
-template <typename Ret, typename T, typename N, typename R> Ret clamp(T a, N min, R max)
-{
-	return (a < min) ? min : ((a > max) ? max : a);
-}
+#include <functional>
+#include <int2.h>
 
 // Row major container for dynamic 2D arrays
+// It has unique_ptr like semantics, in that assignment invalidates the object on the right (technically it just swaps them)
 template<typename T>
 struct Buffer2D
 {
-	struct int2
-	{
-		int x, y;
-	};
-
 	int2 offset;
 	int2 res;
 	T* data;
@@ -73,6 +65,14 @@ struct Buffer2D
 		swap(lhs.offset, rhs.offset);
 	}
 
+	Buffer2D<T> duplicate() const
+	{
+		Buffer2D<T> out(res);
+		memcpy(out.data, data, sizeof(T) * res.x * res.y);
+
+		return out;
+	}
+
 	void resize(int2 newRes)
 	{
 		if (data) delete[] data;
@@ -83,20 +83,40 @@ struct Buffer2D
 
 	T& operator[](int2 p)
 	{
-		p.x = clamp<int>(p.x - offset.x, 0, res.x - 1);
-		p.y = clamp<int>(p.y - offset.y, 0, res.y - 1);
+		p.x = std::clamp<int>(p.x - offset.x, 0, res.x - 1);
+		p.y = std::clamp<int>(p.y - offset.y, 0, res.y - 1);
 
 		return data[p.x + res.x*p.y];
 	}
 	const T& operator[](int2 p) const
 	{
-		p.x = clamp<int>(p.x - offset.x, 0, res.x - 1);
-		p.y = clamp<int>(p.y - offset.y, 0, res.y - 1);
+		p.x = std::clamp<int>(p.x - offset.x, 0, res.x - 1);
+		p.y = std::clamp<int>(p.y - offset.y, 0, res.y - 1);
 
 		return data[p.x + res.x*p.y];
 	}
 
-	void foreach(function<void(int, int2, T&)> f)
+	void foreach(std::function<void(int, int2, T&)> f)
+	{
+		int idx = 0;
+
+		for (int y = 0; y < res.y; y++)
+		{
+			for (int x = 0; x < res.x; x++)
+			{
+				f(idx, { x,y }, data[idx]);
+				idx++;
+			}
+		}
+	}
+	
+	bool contains(int2 px) const
+	{
+		px -= offset;
+		return (px.x >= 0 && px.x < res.x) && (px.y >= 0 && px.y < res.y);
+	}
+
+	void foreach(std::function<void(int, int2, T)> f) const
 	{
 		int idx = 0;
 
@@ -110,16 +130,61 @@ struct Buffer2D
 		}
 	}
 
-	void foreach(function<void(int, int2, T)> f) const
+	// like foreach but you can early exit
+	void iterate(std::function<bool(int, int2, T)> f) const
 	{
 		int idx = 0;
 
-		for (int y = 0; y < res.y; y++)
+		bool exit_loop = false;
+		for (int y = 0; y < res.y && !exit_loop; y++)
 		{
 			for (int x = 0; x < res.x; x++)
 			{
-				f(idx, { x,y }, data[idx]);
+				if (f(idx, { x,y }, data[idx]))
+				{
+					exit_loop = true;
+					break;
+				}
 				idx++;
+			}
+		}
+	}
+
+	void window_map(int radius, std::function<bool(int2, int2, T)> pix_f, std::function<bool(int2)> window_pre_f, std::function<bool(int2)> window_post_f = std::function<bool(int2)>() )
+	{
+		bool exit_loop = false;
+		for (int y = 0; y < res.y && !exit_loop; y++)
+		{
+			for (int x = 0; x < res.x; x++)
+			{
+				if (window_pre_f({ x,y }))
+				{
+					exit_loop = true;
+					break;
+				}
+
+				bool exit_sample = false;
+				for (int rx = -radius; rx <= radius && !exit_sample; rx++)
+				{
+					for (int ry = -radius; ry <= radius; ry++)
+					{
+						// TODO : Move clamp to function
+						int sx = std::clamp<int>((x + rx) - offset.x, 0, res.x - 1);
+						int sy = std::clamp<int>((y + ry) - offset.y, 0, res.y - 1);
+
+						if (pix_f({ sx, sy }, { rx, ry }, data[sx + res.x * sy]))
+						{
+							exit_sample = true;
+							break;
+						}
+					}
+				}
+
+				if (window_post_f({ x,y }))
+				{
+					exit_loop = true;
+					break;
+				}
 			}
 		}
 	}
